bd5c38c689483a64b2bfe2a14189e193fbcc8aff
jdogmcsteezy <jesse.sasquatch.schmidt@gmail.com>
2017-11-01 15:26:59 -0700

    Real init

--- pygameTest.py
+++ pygameTest.py
@@ -1,7 +1,7 @@
 import pygame
 import os
 import sys
-
+speed = 100
 dir_path = os.path.dirname(os.path.realpath(__file__))
 pygame.init()
 screen = pygame.display.set_mode((1000,1000))
@@ -10,29 +10,32 @@ sin = pygame.image.load(dir_path + "/sin.PNG").convert()
 screen.blit(bg, (-100,-100))
 clockScreen = pygame.Surface((545,400))
 clockScreen.fill((255,255,255))
-
 x=0
 x1 = sin.get_width()
 clockScreen.blit(sin,(x,0))
 clockScreen.blit(sin,(x1,0))
 screen.blit(clockScreen,(100,100))
-start = pygame.time.get_ticks()
+starttime = pygame.time.get_ticks()
 done = False
 while done == False:
-	pygame.time.wait(900)
+	pygame.time.get_ticks()
+	while pygame.time.get_ticks() - starttime < speed:
+		pygame.time.wait(1)
+	starttime = pygame.time.get_ticks()
+	#pygame.time.wait(900)
 	clockScreen.blit(sin,(x,0))
 	clockScreen.blit(sin,(x1,0))
 	screen.blit(clockScreen,(100,100))
 	pygame.display.update()
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			done = True
 	x -= 1
 	x1 -= 1
-
 	if (x + sin.get_width()) < 0:
 		x = sin.get_width()
 	elif (x1 + sin.get_width()) < 0:
 		x1 = sin.get_width()
+
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			done = True
 pygame.quit()
 sys.exit()
