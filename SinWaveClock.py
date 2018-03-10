9d958adec663ae1984309635ad07db20f121200d
jdogmcsteezy <jesse.sasquatch.schmidt@gmail.com>
2018-03-10 11:51:54 -0800

    Update Colors and fonts

--- SinWaveClock.py
+++ SinWaveClock.py
@@ -5,44 +5,50 @@ import math
 from os import path
 
 class SinWaveClock(pygame.Surface):
-  def __init__(self, height, width):
+  def __init__(self, width, height):
     pygame.Surface.__init__(self,(width, height))
     pygame.font.init()
     # All values that can edit the sin wave clock
-    self.sin_Length = self.get_width() + 5
+    self.sin_Length = self.get_width()
     # If zero it will just be a line
     self.sin_DotProximity = 10
     # .5 is 1 second period
     self.sin_Period = .25
     self.sin_Frequency = 4
-    self.sin_Amplitude = 50 # in px
-    self.sin_YDisplacment = 75
-    self.sin_XDisplacment = -30
+    self.sin_Amplitude = int(height * .166667)
+    self.sin_YDisplacment = int(height * .25)
+    self.sin_XDisplacment = int(width * .005)
     # Color for dots in the future
-    self.sin_LnColorPrimary = (198,225,234)
+    self.sin_LnColorPrimary = (186, 111, 23)
     # Clolor for dots in the past
-    self.sin_LnColorSecondary = (158,0,49)
+    self.sin_LnColorSecondary = (39,40,34)
     # Size of dots
-    self.sin_LnRadius = 4
+    self.sin_LnRadius = int(height * .01633333)
+    self.sin_SecondsDotRadius = int(height * .026667)
+    self.sin_SecondsYDisplacment = int(height * -.06667)
+    self.sin_SecondsXDisplacment = int(width * -.055)
     self.bgColor = (0,0,0)
     self.bgSurface = pygame.Surface((width, height))
     # FONTS
-    self.secondsFont = pygame.font.Font((path.dirname(path.realpath(__file__)) + '/1979_dot_matrix.ttf'), 30)
-    self.dateFont = pygame.font.Font((path.dirname(path.realpath(__file__)) + '/1979_dot_matrix.ttf'), 50)
-    self.timeFont = pygame.font.Font((path.dirname(path.realpath(__file__)) + '/charlie_dotted.ttf'), 150)
-    self.fontColor = (255,255,255)
+    self.fonts_path = path.join(path.dirname(path.realpath(__file__)), 'Fonts')
+    self.secondsFont = pygame.font.Font(path.join(self.fonts_path, '1979_dot_matrix.ttf'), int(height * .08))
+    self.dateFont = pygame.font.Font(path.join(self.fonts_path, 'OpenSans-Regular.ttf'), int(height * .25))
+    self.timeFont = pygame.font.Font(path.join(self.fonts_path, 'OpenSans-Regular.ttf'), int(height * .25))
+    self.fontColor = (242,242,242)
     self.previousSec = int(time.time() % 60)
     self.previousTime = time.strftime('%I:%M %p')
     self.previousDate = time.strftime('%a, %b %d')
-    self.secondsSurface = self.secondsFont.render(str(self.previousSec), True, (255,255,255))
-    self.dateSurface = self.dateFont.render(str(self.previousDate), True, (255,255,255))
-    self.timeSurface = self.timeFont.render(str(self.previousTime), True, (255,255,255))
+    self.secondsSurface = self.secondsFont.render(str(self.previousSec), True, (242,242,242))
+    self.dateSurface = self.dateFont.render(str(self.previousDate), True, (242,242,242))
+    self.timeSurface = self.timeFont.render(str(self.previousTime), True, (242,242,242))
+    self.dateSurfaceXYOffset = (int(width * .01), int(height * -.03333333))
+    self.timeSurfaceXYOffset = (int(width * .008), int(height * .216667))
     self.bgSurface.fill(self.bgColor)
     self.bgSurface.convert()
     self.blit(self.bgSurface, (0,0))
     self.convert()
 
-  def update(self):
+  def Update(self):
     currentTime = time.strftime('%I:%M %p')
     currentDate = time.strftime('%a, %b %d')
     currentSecond = int(time.time() % 60)
@@ -68,14 +74,14 @@ class SinWaveClock(pygame.Surface):
         pygame.draw.circle(self, self.sin_LnColorPrimary, (x + self.sin_XDisplacment,y), self.sin_LnRadius)
         if (x - (self.sin_Length)/2) < self.sin_DotProximity/2 :
           # Draw dots behind seconds counter if behind the middle mark of sinwave.
-          pygame.draw.circle(self, self.sin_LnColorSecondary, (x + self.sin_XDisplacment,y), 4)
+          pygame.draw.circle(self, self.sin_LnColorSecondary, (x + self.sin_XDisplacment,y), self.sin_LnRadius)
         # Draws larger second counter Dot
         if (abs(x - (self.sin_Length)/2) < self.sin_DotProximity/2) and (y < self.sin_YDisplacment + (self.get_height()/2)) :
-          pygame.draw.circle(self, self.sin_LnColorPrimary, (x + self.sin_XDisplacment,y), 8)
-          self.blit(self.secondsSurface,(x + self.sin_XDisplacment - 55,y - 20))
+          pygame.draw.circle(self, self.sin_LnColorPrimary, (x + self.sin_XDisplacment,y), self.sin_SecondsDotRadius)
+          self.blit(self.secondsSurface,(x + self.sin_XDisplacment + self.sin_SecondsXDisplacment ,y + self.sin_SecondsYDisplacment))
         elif (abs(x - (self.sin_Length)/2) < self.sin_DotProximity/2) and (y >= self.sin_YDisplacment + (self.get_height()/2)):
-          pygame.draw.circle(self, self.sin_LnColorSecondary, (x + self.sin_XDisplacment,y), 8)
-          self.blit(self.secondsSurface,(x + self.sin_XDisplacment - 55,y - 20))
-        self.blit(self.dateSurface, (10, 10))
-        self.blit(self.timeSurface, (10, 45))
+          pygame.draw.circle(self, self.sin_LnColorSecondary, (x + self.sin_XDisplacment,y), self.sin_SecondsDotRadius)
+          self.blit(self.secondsSurface,(x + self.sin_XDisplacment + self.sin_SecondsXDisplacment,y + self.sin_SecondsYDisplacment))
+        self.blit(self.dateSurface, self.dateSurfaceXYOffset)
+        self.blit(self.timeSurface, self.timeSurfaceXYOffset)
     return self
--- testSinWaveCLock.py
+++ testSinWaveCLock.py
@@ -4,15 +4,15 @@ import pygame
 def main():
     pygame.init()
     clock = pygame.time.Clock()
-    testScreen = pygame.display.set_mode((1000, 1000))
+    testScreen = pygame.display.set_mode((1000, 300))
     testScreen.fill((0,0,0))
-    sin = SinWaveClock.SinWaveClock(300, 1000)
+    sin = SinWaveClock.SinWaveClock(1000, 300)
     testScreen.blit(sin,(0,0))
     pygame.display.update()
     run = True
     while(run):
         clock.tick(26)
-        testScreen.blit(sin.update(), (0, 0))
+        testScreen.blit(sin.Update(), (0, 0))
         pygame.display.update()
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN:
